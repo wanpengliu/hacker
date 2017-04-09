aa7dfdaf0c480383b7a02bb8a513f99cc9a95c9a
Wanpeng <wanpengliu@gmail.com>
2017-04-09 21:08:30 +0100

    first version

---
+++ src/main/java/com/wanpeng/hacker/HackerProjectApplication.java
@@ -0,0 +1,12 @@
+package com.wanpeng.hacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerProjectApplication.class, args);
+	}
+}
